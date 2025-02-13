dbd3a9c48516b76b839d5e6d4e6e5248fbcc1921
Akash Sharma <akashsharma@192.168.1.3>
2025-02-13 22:50:19 +0530

    feat: implemented dashboard page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subset: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
           <Header />
           <main className="min-h-screen">{children}</main>
         </body>
+        <Toaster richColors />
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-grey-600">
             Made with ❤️ by Akash Sharma
