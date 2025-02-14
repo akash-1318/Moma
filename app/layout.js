618e0c1adc1d9d968c9db28a867e5f5ac53a36a4
Akash Sharma <akashsharma@192.168.1.3>
2025-02-14 18:49:12 +0530

    fix: fixed build issue

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
-const inter = Inter({ subset: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Moma",
