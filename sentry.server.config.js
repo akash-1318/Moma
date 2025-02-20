e15fc481d73c73ddce74a644fbf6553f048f69e1
Akash Sharma <akashsharma@192.168.1.7>
2025-02-20 14:24:07 +0530

    feat: integrated sentry for error handling

---
+++ sentry.server.config.js
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cefbfdb09b13284989a2ecb0ec71911c@o4508850596675584.ingest.us.sentry.io/4508850599231488",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
